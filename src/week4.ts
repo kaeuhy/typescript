6d5b81d0f6fb4a844b03bc1e86b134e02f46ae1d
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-11-17 11:27:30 +0900

    chore: 3주차 세미나 실습 코드 작성

--- src/week4.ts
+++ src/week4.ts
@@ -38,20 +38,59 @@
 //
 //
 
-interface User {
+// interface User {
+//     id: string;
+//     name: string;
+// }
+//
+// let seho: User;
+
+
+// 라이브 코딩 중첩 삼항연산자
+// interface Person {
+//     name: string,
+//     age: number,
+// }
+//
+// interface Developer {
+//     name: string,
+//     skill: string,
+// }
+//
+// function introduce(someone: Person | Developer): void {
+//     "skill" in someone
+//         ? console.log("skill이 있습니다.")
+//         : "age" in someone
+//             ? console.log("age가 있습니다.")
+//             : console.log("둘 다 해당하지 않습니다")
+// }
+//
+// introduce({name: '은현', skill: '자바스크립트' });
+// introduce({name: '은현', book: '자바스크립트'})
+
+
+// type del = string | number | boolean;
+//
+// function logMessage(msg: del) {
+//     console.log(msg);
+// }
+//
+// let m1: del = 'hello';
+// let m2: del = 123;
+
+type User = {
     id: string;
     name: string;
 }
 
-let seho: User;
+interface Admin extends User {
+    role: string;
+}
 
+const admin: Admin = {
+    id: 'asd',
+    name: 'root',
+    role: 'a'
+};
 
-
-
-
-
-
-
-
-
-
+console.log(admin.id);
